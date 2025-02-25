21c810a35ea829e3f0223011cd05f9cea924f2e0
Maria Eduarda Torres Pereira <m.torresp0001@gmail.com>
2025-02-25 11:05:48 -0300

    Add files via upload

---
+++ AMS-ED-2025-Entregas-S1-S2/AMS-ED-2025-Entregas-S1/AMS-ED-2025-Entregas-S1-B1/AMS-ED-2025-Entregas-S1-B1-Atividade1/AMS-ED-2025-Entregas-S1-B1-Atividade1-CodFonte.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void aplicadesc(float *ptrvlrprod, float *ptrvlrprodcdesc, float *ptrporcdesc)
+{
+    *ptrvlrprodcdesc = *ptrvlrprod * (1 - (*ptrporcdesc / 100));
+}
+
+int main()
+{
+
+    float vlrprod, vlrprodcdesc, porcdesc;
+    char rsp;
+
+    printf("Insira o valor do produto: ");
+    scanf("%f", &vlrprod);
+
+    printf("Deseja aplicar algum desconto ao valor do produto? (s/n)");
+    scanf(" %c", &rsp);
+
+    if (rsp == 's' || rsp == 'S')
+    {
+        printf("Aplique o porcentual do desconto (não é necessário o '%'): ");
+        scanf("%f", &porcdesc);
+
+        aplicadesc(&vlrprod, &vlrprodcdesc, &porcdesc);
+
+        printf("Valor original do produto: R$%.2f\n", vlrprod);
+
+        printf("Valor com o desconto: R$%.2f\n", vlrprodcdesc);
+
+        printf("Desconto de: %.2f\n", porcdesc);
+    }
+    else if (rsp == 'n' || rsp == 'N')
+    {
+        printf("Valor original do produto: R$%.2f\n", vlrprod);
+    }
+    else
+    {
+        printf("Resposta inválida!!!");
+    }
+
+    return 0;
+}
